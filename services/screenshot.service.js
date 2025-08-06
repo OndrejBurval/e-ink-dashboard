382905893c8cff16496ea0a257203f4f9af9562a
Ondřej Buřval <ondrej.burval@outlook.cz>
2025-08-06 08:31:46 +0200

    [FTR] Add sharp dependency, enhance display styles, and update screenshot service functionality

--- services/screenshot.service.js
+++ services/screenshot.service.js
@@ -1,6 +1,10 @@
-import puppeteer from 'puppeteer-core';
-import { SCREENSHOT_LOCATION } from '../utils/config.js';
-import chalk from 'chalk';
+import puppeteer from "puppeteer-core";
+import {
+  SCREENSHOT_LOCATION,
+  SCREENSHOT_LOCATION_OUTPUT,
+} from "../utils/config.js";
+import chalk from "chalk";
+import sharp from "sharp";
 
 class ScreenshotService {
   constructor() {
@@ -10,16 +14,18 @@ class ScreenshotService {
 
   async initBrowser() {
     if (!this.browser) {
-      console.info(chalk.yellow('⏱️  Initializing browser...'));
+      console.info(chalk.yellow("⏱️  Initializing browser..."));
       try {
         this.browser = await puppeteer.launch({
           headless: true,
-          args: ['--no-sandbox', '--disable-setuid-sandbox'],
-          executablePath: '/usr/bin/chromium-browser',
+          args: ["--no-sandbox", "--disable-setuid-sandbox"],
+          executablePath: "/usr/bin/chromium-browser",
         });
         this.page = await this.browser.newPage();
       } catch (error) {
-        console.error(chalk.red('❌ Browser initialization error:', error.message));
+        console.error(
+          chalk.red("❌ Browser initialization error:", error.message)
+        );
         throw error;
       }
 
@@ -29,7 +35,7 @@ class ScreenshotService {
   }
 
   async takeScreenshot() {
-    const TIMER_LABEL = '⏱️  Screenshot timer';
+    const TIMER_LABEL = "⏱️  Screenshot timer";
     try {
       console.time(TIMER_LABEL);
       const { page } = await this.initBrowser();
@@ -37,17 +43,46 @@ class ScreenshotService {
       await page.goto("http://localhost:8000/");
       await page.waitForSelector("#display");
 
+      await page.setViewport({
+        width: 1920,
+        height: 1080,
+        deviceScaleFactor: 2,
+      });
+
       const element = await page.$("#display");
 
-      await element.screenshot({ type: 'webp', path: `./public/${SCREENSHOT_LOCATION}` });
-      console.timeEnd(TIMER_LABEL);
-      const date = new Date().toLocaleTimeString('cs-CZ', { hour: '2-digit', minute: '2-digit', second: '2-digit', month: '2-digit', day: '2-digit', year: 'numeric' });
-      console.log(chalk.green(`✅ Screenshot saved: ${SCREENSHOT_LOCATION} (${date})`));
-      return { success: true };
+      await element.screenshot({
+        type: "webp",
+        path: `./public/${SCREENSHOT_LOCATION}`,
+        quality: 100,
+        omitBackground: false,
+      });
 
+      await sharp(`./public/${SCREENSHOT_LOCATION}`)
+        .greyscale()
+        .threshold(128)
+        .toFormat("png")
+        .toFile(`./public/${SCREENSHOT_LOCATION_OUTPUT}`, (err, info) => {
+          if (err) throw err;
+          console.log("Saved BMP:", info);
+        });
+
+      console.timeEnd(TIMER_LABEL);
+      const date = new Date().toLocaleTimeString("cs-CZ", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        month: "2-digit",
+        day: "2-digit",
+        year: "numeric",
+      });
+      console.log(
+        chalk.green(`✅ Screenshot saved: ${SCREENSHOT_LOCATION} (${date})`)
+      );
+      return { success: true };
     } catch (error) {
       console.timeEnd(TIMER_LABEL);
-      console.log(chalk.red('Screenshot error:', error.message));
+      console.log(chalk.red("Screenshot error:", error.message));
       return { success: false, error: error.message };
     }
   }
--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,4 @@
 export const WEATHER_URL = "https://api.open-meteo.com/v1/forecast";
 export const CALENDAR_MAX_EVENTS = 2;
-export const SCREENSHOT_LOCATION = "/images/output.webp";
+export const SCREENSHOT_LOCATION = "/images/screenshot.webp";
+export const SCREENSHOT_LOCATION_OUTPUT = "/images/output.png";
