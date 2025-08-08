27ec3adb1cd99948ab2ca3a059f85f49ae5a3808
Ondřej Buřval <ondrej.burval@outlook.cz>
2025-08-08 19:04:01 +0200

    [FTR] display.py

--- cli/python/display.py
+++ cli/python/display.py
@@ -1,21 +1,45 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
 import sys
+import os
+
+# Add local lib directory to path
+libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import time
+import logging
 from PIL import Image
+from waveshare_epd import epd2in13_V4
 
-sys.path.append('./waveshare/lib/waveshare_epd')
-from epd2in13_V4 import EPD  # Import the EPD class from the module
+# Logging setup
+logging.basicConfig(level=logging.DEBUG)
 
-def display_bmp_on_epaper(bmp_path):
-    epd = EPD()  # Use the imported EPD class
+try:
+    logging.info("Display output.bmp on e-paper")
+
+    # Initialize e-paper
+    epd = epd2in13_V4.EPD()
     epd.init()
     epd.Clear(0xFF)
 
-    image = Image.open(bmp_path).convert('1')
-    image = image.resize((epd.width, epd.height))
+    # Path setup
+    picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../public/images/')
+    image_path = os.path.join(picdir, 'output.bmp')
+
+    # Load and display image
+    image = Image.open(image_path)
     epd.display(epd.getbuffer(image))
+    time.sleep(5)
+
+    # Optional: sleep mode to save power
     epd.sleep()
 
-if __name__ == "__main__":
-    bmp_path = sys.argv[1]
-    display_bmp_on_epaper(bmp_path)
-    print('Image displayed')
-    sys.stdout.flush()
+except IOError as e:
+    logging.error(e)
+
+except KeyboardInterrupt:
+    logging.info("Interrupted by user")
+    epd2in13_V4.epdconfig.module_exit(cleanup=True)
+    exit()
